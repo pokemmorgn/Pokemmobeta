d5901fd050b7271951dbc1752a59926e888f543c
pokemmorgn <pokemmorgn@gmail.com>
2025-06-14 00:46:52 +0200

    Update WorldRoom.js

--- rooms/WorldRoom.js
+++ rooms/WorldRoom.js
@@ -1,41 +1,44 @@
 const colyseus = require("colyseus");
+const Schema = require("@colyseus/schema").Schema;
+const type = require("@colyseus/schema").type;
 
-class Player {
-    constructor(x = 400, y = 300) {
-        this.x = x;
-        this.y = y;
-    }
-}
-
-class State {
+// Classe joueur synchronisée
+class Player extends Schema {
     constructor() {
-        this.players = {};
-    }
-    createPlayer(sessionId) {
-        this.players[sessionId] = new Player();
-    }
-    removePlayer(sessionId) {
-        delete this.players[sessionId];
-    }
-    movePlayer(sessionId, x, y) {
-        if (this.players[sessionId]) {
-            this.players[sessionId].x = x;
-            this.players[sessionId].y = y;
-        }
+        super();
+        this.x = 400;
+        this.y = 300;
     }
 }
+type("number")(Player.prototype, "x");
+type("number")(Player.prototype, "y");
 
+// Classe état global
+class State extends Schema {
+    constructor() {
+        super();
+        this.players = new Map();
+    }
+}
+type({ map: Player })(State.prototype, "players");
+
+// Room principale
 exports.WorldRoom = class extends colyseus.Room {
     onCreate() {
         this.setState(new State());
         this.onMessage("*", (client, data) => {
-            this.state.movePlayer(client.sessionId, data.x, data.y);
+            const player = this.state.players.get(client.sessionId);
+            if (player) {
+                if (typeof data.x === "number") player.x = data.x;
+                if (typeof data.y === "number") player.y = data.y;
+            }
         });
     }
     onJoin(client) {
-        this.state.createPlayer(client.sessionId);
+        const player = new Player();
+        this.state.players.set(client.sessionId, player);
     }
     onLeave(client) {
-        this.state.removePlayer(client.sessionId);
+        this.state.players.delete(client.sessionId);
     }
 };
